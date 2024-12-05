6338e968a6ff483cf4af6621f4f734271b8b3ca8
ezequias.ferreira <ezequias.ferreira@ifood.com.br>
2024-12-05 20:01:46 -0300

    Fix date tables

--- src/main/java/com/delogic/tickets/domain/Date.java
+++ src/main/java/com/delogic/tickets/domain/Date.java
@@ -11,12 +11,21 @@ public class Date {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "date_id")
     private Long id;
-
     private java.sql.Date calendarDate;
+
+    @Column(name = "`day`")
     private String day;
+
+    @Column(name = "`week`")
     private Integer week;
+
+    @Column(name = "`month`")
     private String month;
+
+    @Column(name = "`quarter`")
     private Integer quarter;
+
+    @Column(name = "`year`")
     private Integer year;
     private Boolean holidayFlag;
 }
