259e68f5b19e62bc6a7b8b5f4a82860649fb94b4
ezequias.ferreira <ezequias.ferreira@ifood.com.br>
2024-12-07 15:44:58 -0300

    Fix config

--- src/main/java/com/delogic/tickets/config/TicketsConfig.java
+++ src/main/java/com/delogic/tickets/config/TicketsConfig.java
@@ -2,8 +2,6 @@ package com.delogic.tickets.config;
 
 import com.delogic.tickets.mapper.*;
 import com.delogic.tickets.mapper.impl.*;
-import com.delogic.tickets.repository.*;
-import com.delogic.tickets.service.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,18 +14,8 @@ public class TicketsConfig {
     }
 
     @Bean
-    public UserService userService(UserRepository userRepository, UserMapper userMapper) {
-        return new UserService(userRepository, userMapper);
-    }
-
-    @Bean
     public EventMapper eventMapper() {
         return new EventMapperImpl();
-    }
-
-    @Bean
-    public EventService eventService(EventRepository eventRepository, EventMapper eventMapper) {
-        return new EventService(eventRepository, eventMapper);
     }
 
     @Bean
@@ -36,18 +24,8 @@ public class TicketsConfig {
     }
 
     @Bean
-    public VenueService venueService(VenueRepository venueRepository, VenueMapper venueMapper) {
-        return new VenueService(venueRepository, venueMapper);
-    }
-
-    @Bean
     public CategoryMapper categoryMapper() {
         return new CategoryMapperImpl();
-    }
-
-    @Bean
-    public CategoryService categoryService(CategoryRepository categoryRepository, CategoryMapper categoryMapper) {
-        return new CategoryService(categoryRepository, categoryMapper);
     }
 
     @Bean
@@ -56,27 +34,12 @@ public class TicketsConfig {
     }
 
     @Bean
-    public DateService dateService(DateRepository dateRepository, DateMapper dateMapper) {
-        return new DateService(dateRepository, dateMapper);
-    }
-
-    @Bean
     public ListingMapper listingMapper() {
         return new ListingMapperImpl();
     }
 
     @Bean
-    public ListingService listingService(ListingRepository listingRepository, ListingMapper listingMapper) {
-        return new ListingService(listingRepository, listingMapper);
-    }
-
-    @Bean
     public SaleMapper saleMapper() {
         return new SaleMapperImpl();
-    }
-
-    @Bean
-    public SaleService saleService(SaleRepository saleRepository, SaleMapper saleMapper) {
-        return new SaleService(saleRepository, saleMapper);
     }
 }
